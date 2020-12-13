0057ea7f45553de41934921fe833a787785ae17e
ZachKopec <zachkopec@optonline.net>
2020-12-13 13:21:50 -0500

    Function start and variables

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -4,3 +4,14 @@ var tableData = data;
 // YOUR CODE HERE!
 var button = d3.select("#filter-btn")
 var form = d3.select("#form")
+
+button.on("click", submission);
+button.on("submit", submission);
+
+function submission() {
+
+    d3.event.preventDefault();
+
+    var inputElement = d3.select("#datetime")
+
+}
