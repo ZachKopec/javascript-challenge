bc75d29b520a9dc8d53a94a55d27ebb2763fe98e
ZachKopec <zachkopec@optonline.net>
2020-12-14 22:47:47 -0500

    added more input forms

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -12,15 +12,43 @@ function submission() {
 
     d3.event.preventDefault();
 
-    var inputElement = d3.select("#datetime")
-    var inputValue = inputElement.property("value")
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
 
-    console.log(inputValue)
-    console.log(tableData)
+    var inputElement2 = d3.select("#city");
+    var inputValue2 = inputElement2.property("value");
 
-    var filteredData = tableData.filter(item => item.datetime === inputValue);
+    var inputElement3 = d3.select("#city");
+    var inputValue3 = inputElement3.property("value");
+
+    var inputElement4 = d3.select("#city");
+    var inputValue4 = inputElement4.property("value");
+
+    var inputElement5 = d3.select("#city");
+    var inputValue5 = inputElement5.property("value");
+
+    console.log(inputValue);
+    console.log(inputValue2);
+    console.log(inputValue3);
+    console.log(inputValue4);
+    console.log(inputValue5);
+    console.log(tableData);
+    
+    if (inputValue.length > 0 && inputValue2.length > 0) {
+        var filteredData = tableData.filter(item => item.datetime === inputValue && item.city === inputValue2);
+    } else if (inputValue.length > 0 && inputValue2.length === 0) {
+        var filteredData = tableData.filter(item => item.datetime === inputValue);
+    } else if (inputValue.length === 0 && inputValue2.length > 0) {
+        var filteredData = tableData.filter(item => item.city === inputValue2);
+    }
+    // var filteredData2 = tableData.filter(item => item.city === inputValue2);
 
     console.log(filteredData);
+    // console.log(filteredData2);
+
+    // if (filteredData.length < 1) {
+    //     alert("No recorded instances for this date.")
+    // };
 
     var list = d3.select("#tbody");
 
