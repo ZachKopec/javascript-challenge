e2c79ac8b5c2d490b118781b445a8644d9b71ce8
ZachKopec <zachkopec@optonline.net>
2020-12-20 01:14:46 -0500

    Forgot to push last changes to file, included table when page is loaded

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,65 +1,49 @@
-// from data.js
-var tableData = data;
+// from data.js
+var tableData = data;
 
-// YOUR CODE HERE!
-var button = d3.select("#filter-btn")
-var form = d3.select("#form")
+// YOUR CODE HERE!
+var button = d3.select("#filter-btn")
+var form = d3.select("#form")
 
-button.on("click", submission);
-form.on("submit", submission);
+button.on("click", submission);
+form.on("submit", submission);
 
-function submission() {
+var list2 = d3.select("#tbody");
 
-    d3.event.preventDefault();
+list2.html("");
 
-    var inputElement = d3.select("#datetime");
-    var inputValue = inputElement.property("value");
+data.forEach((data) => {
+    var row = list2.append("tr");
+    Object.entries(data).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
 
-    var inputElement2 = d3.select("#city");
-    var inputValue2 = inputElement2.property("value");
+function submission() {
 
-    var inputElement3 = d3.select("#city");
-    var inputValue3 = inputElement3.property("value");
+    d3.event.preventDefault();
 
-    var inputElement4 = d3.select("#city");
-    var inputValue4 = inputElement4.property("value");
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
 
-    var inputElement5 = d3.select("#city");
-    var inputValue5 = inputElement5.property("value");
+    console.log(inputValue);
+    console.log(tableData);
+    
+    var filteredData = tableData.filter(item => item.datetime === inputValue);
 
-    console.log(inputValue);
-    console.log(inputValue2);
-    console.log(inputValue3);
-    console.log(inputValue4);
-    console.log(inputValue5);
-    console.log(tableData);
-    
-    if (inputValue.length > 0 && inputValue2.length > 0) {
-        var filteredData = tableData.filter(item => item.datetime === inputValue && item.city === inputValue2);
-    } else if (inputValue.length > 0 && inputValue2.length === 0) {
-        var filteredData = tableData.filter(item => item.datetime === inputValue);
-    } else if (inputValue.length === 0 && inputValue2.length > 0) {
-        var filteredData = tableData.filter(item => item.city === inputValue2);
-    }
-    // var filteredData2 = tableData.filter(item => item.city === inputValue2);
+    console.log(filteredData);
 
-    console.log(filteredData);
-    // console.log(filteredData2);
+    var list = d3.select("#tbody");
 
-    // if (filteredData.length < 1) {
-    //     alert("No recorded instances for this date.")
-    // };
-
-    var list = d3.select("#tbody");
-
-    list.html("");
-    
-    filteredData.forEach((data) => {
-        var row = list.append("tr");
-        Object.entries(data).forEach(([key, value]) => {
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });
+    list.html("");
+    
+    filteredData.forEach((data) => {
+        var row = list.append("tr");
+        Object.entries(data).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
 
 }
